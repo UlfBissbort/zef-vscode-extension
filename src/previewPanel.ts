954d9ba7a9737bbfa5446b068d9ce62dd8af32fb
Ulf <ulf.bissbort@gmail.com>
2026-02-11 13:40:55 +0800

    icon

--- src/previewPanel.ts
+++ src/previewPanel.ts
@@ -3243,7 +3243,7 @@ function getWebviewContent(renderedHtml: string, existingOutputs: { [blockId: nu
                     var mermaidExpandBtn = document.createElement('button');
                     mermaidExpandBtn.className = 'mermaid-export-btn';
                     mermaidExpandBtn.title = 'Pop Out';
-                    mermaidExpandBtn.innerHTML = '<svg viewBox="0 0 24 24"><polyline points="15 3 21 3 21 9"></polyline><polyline points="9 21 3 21 3 15"></polyline><line x1="21" y1="3" x2="14" y2="10"></line><line x1="3" y1="21" x2="10" y2="14"></line></svg>';
+                    mermaidExpandBtn.innerHTML = '<svg viewBox="0 0 24 24"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>';
                     mermaidExpandBtn.onclick = (function(container) {
                         return function() {
                             var rendered = container.querySelector('.mermaid-rendered .mermaid');
